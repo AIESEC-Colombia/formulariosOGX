653192f2c18a7fe4a451e0ddfadaa35e1ea9150f
julian estrada jaramillo <julian estrada jaramillo>
2016-12-08 09:27:11 -0500

    carga de los fromularios

--- protected/controllers/VoluntarioGlobalController.php
+++ protected/controllers/VoluntarioGlobalController.php
@@ -64,7 +64,7 @@ class VoluntarioGlobalController extends Controller
 		  new PodioCategoryItemField(array("external_id" => "email-2", "values" => $Json['txtmail'])),
 		  new PodioCategoryItemField(array("external_id" => "iduniversity", "values" => $Json['valUniversidad'])),
 		  new PodioCategoryItemField(array("external_id" => "university", "values" => $Json['nombreUniversidad'])),
-		  new PodioCategoryItemField(array("external_id" => "howmet-2", "values" => (int)$Json['lstConocioOrganizacion']))
+		  new PodioCategoryItemField(array("external_id" => "howmet-2", "values" => $Json['lstConocioOrganizacion']))
 		  ));
 
 		$item = new PodioItem(array(
