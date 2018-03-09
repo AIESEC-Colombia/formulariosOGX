dc7af3fc299e380b13dc25bd71839cb8e6752a8c
AIESEC-Colombia <30851212+AIESEC-Colombia@users.noreply.github.com>
2018-03-09 11:56:51 -0500

    Añadir 3 Forms de Anuncios Redes Sociales
    Un formulario nuevo por producto, donde la pregunta de como conocio la organizacion esté oculta, y por default la repsuesta en podio sea Lead Prioritarios

---
+++ protected/views/voluntarioGlobalAds/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <style type="text/css">
+      @font-face { font-family: "Lato-Light";
+                src: url('assets/Lato-Light.ttf');
+                 }
+      html{
+        font-family: "Lato-Light";
+        background-color: white;
+      }
+      body{
+        background-color: white;
+      }
+    </style>
+  </head>
+  <body>
+  <div class="container">
+    
+      <div class="col s12 m7">
+        <h2 class="header" id="title" style="background-color: '#f85a40'">Voluntario Global</h2>
+      </div>
+      <div class="col s12 m12">
+       Gracias por visitiar nuestro programa de Voluntario Global, con este formulario se registrara en nuestra plataforma de oportunidades sin compromiso. Luego de completar su registro nos pondremos en contacto para asesorarlo en su búsqueda y resolución de inquietudes.
+      </div>
+      <div class="input-field col s12">
+         <i class="material-icons prefix">account_circle</i>
+         <input id="txtFirstName" type="text" class="validate">
+         <label for="txtFirstName">Primer nombre*</label>
+      </div>
+      <div class="input-field col s12">
+         <i class="material-icons prefix">perm_identity</i>
+         <input id="txtLastName" type="text" class="validate">
+         <label for="txtLastName">Apellido*</label>
+      </div>
+      <div class="input-field col s12">
+         <i class="material-icons prefix">phone</i>
+         <input id="txtPhone" type="text" class="validate">
+         <label for="txtPhone">Teléfono*</label>
+      </div>
+      <div class="input-field col s12">
+         <i class="material-icons prefix">settings_cell</i>
+         <input id="txtMobil" type="text" class="validate">
+         <label for="txtMobil">Celular*</label>
+      </div>
+      <div class="input-field col s12">
+        <i class="material-icons prefix">email</i>
+        <input id="txtmail" type="email" class="validate">
+        <label for="email">Email*</label> 
+      </div>
+      <div class="input-field col s12">
+        <i class="material-icons prefix" >email</i>
+         <select class="listas" id="lstUniversidad">
+           <option value="" disabled selected>Seleccione una universidad</option>             
+         </select>
+         <label>Universidad*</label>
+     </div>
+      <div class="input-field col s12">
+        <i class="material-icons prefix">vpn_key</i>
+        <input id="txtPassword" type="password" class="validate">
+        <label for="txtPassword">Clave*</label>          
+      </div>
+      <div class="input-field col s12">
+        <i class="material-icons prefix">vpn_key</i>
+        <input id="txtSecondPassword" type="password" class="validate">
+        <label for="txtSecondPassword">Repita la Clave*</label>
+      </div>
+      
+
+     <div class="input-field col s12">
+        <i class="material-icons prefix">flight_land</i>
+         <select class="listas" id="lstFechaViaje">
+           <option value="" disabled selected>Cuando crees que sería la fecha de viaje?</option>
+           <option value="1">De hoy a 6 meses</option>
+           <option value="2">De 6 meses un año</option>
+           <option value="3">Sin fecha fija pero quiero conocer más</option>        
+         </select>
+         <label>¿Cuando crees que sería la fecha de viaje?*</label>
+     </div>
+
+     <div class="input-field col s12">
+        <i class="material-icons prefix">group</i>
+         <select class="listas" id="lstContactoPreferencia">
+           <option value="" disabled selected>Preferencia de Contacto?</option>
+           <option value="1">Correo Electrónico</option>
+           <option value="2">Llamada</option>
+           <option value="3">Whatsapp</option>        
+         </select>
+         <label>¿Preferencia de Contacto?*</label>
+     </div>
+
+
+     
+     <div class="input-field col s12">          
+        <input type="checkbox" id="rbAceptoTerminos" />
+        <label for="rbAceptoTerminos">Estoy de acuerdo con los <a id="btnTerminosCondiciones"> términos y condiciones de privacidad </a></label>
+     </div>
+
+    <div class="input-field col s12">
+    <a class="waves-effect waves-light btn" id=btnIngresar style="background-color: '#f85a40'">Registrarse</a>
+    </div>
+  
+  </div>
+     <style type="text/css">
+      @font-face { font-family: "Lato-Light";
+                src: url('<?php echo Yii::app()->request->baseUrl; ?>/assets/Lato-Light.ttf');
+                 }
+      html,body{
+        font-family: "Lato-Light";
+        color: #5d5d5d;
+      }
+      #btnIngresar{
+        background-color: #f85a40; 
+      }
+      #title{
+        color: #f85a40; 
+      }
+      .material-icons{
+        color: #f85a40; 
+      }
+    </style>
+   <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/voluntarioGlobalAds.js"></script>
+  </body>
+</html>
